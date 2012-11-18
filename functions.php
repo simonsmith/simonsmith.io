cb8ddd164bd11a7b4a1b2ac9bf914c244645e1c4
Simon Smith <simon@interlopers.net>
2012-11-18 11:57:17 +0000

    Add functions from old theme

---
+++ functions.php
@@ -0,0 +1,94 @@
+<?php
+
+    add_theme_support('post-thumbnails', array('post', 'work'));
+
+    function create_post_type() {
+        register_post_type('work',
+            array(
+                'labels' => array(
+                    'name' => __('Work'),
+                    'singular_name' => __('Work')
+                ),
+                'public' => true,
+                'has_archive' => true,
+                'supports' => array('title', 'editor', 'thumbnail', 'excerpt', 'comments', 'revisions', 'custom-fields'),
+                'taxonomies' => array('category', 'post_tag'),
+                'menu_position' => 5,
+                'rewrite' => true
+            )
+        );
+    }
+
+    function new_excerpt_length($length) {
+        return 20;
+    }
+
+    function new_excerpt_more($more) {
+        return '...';
+    }
+
+    add_filter('excerpt_more', 'new_excerpt_more');
+    add_filter('excerpt_length', 'new_excerpt_length');
+    add_action('init', 'create_post_type');
+
+    function commentLayout($comment, $args, $depth) {
+
+        $GLOBALS['comment'] = $comment;
+        switch ($comment->comment_type) :
+            case 'pingback' :
+            case 'trackback' :
+                ?>
+            <li class="post pingback">
+                <p><?php _e('Pingback:'); ?> <?php comment_author_link(); ?><?php edit_comment_link(__('Edit'), '<span class="edit-link">', '</span>'); ?></p>
+                <?php
+                break;
+            default :
+                ?>
+            <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+                <article id="comment-<?php comment_ID(); ?>" class="comment">
+                    <footer class="comment-meta">
+                        <?php
+                        $avatar_size = 40;
+                        if ('0' != $comment->comment_parent) $avatar_size = 40;
+
+                        echo get_avatar($comment, $avatar_size);
+                        ?>
+
+                        <div class="comment-author vcard">
+                            <?php
+                            /* translators: 1: comment author, 2: date and time */
+                            printf(__('%1$s %2$s'),
+                                sprintf('<span class="fn">%s</span>', get_comment_author_link()),
+                                sprintf('<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
+                                    esc_url(get_comment_link($comment->comment_ID)),
+                                    get_comment_time('c'),
+                                    /* translators: 1: date, 2: time */
+                                    sprintf(__('%1$s at %2$s', 'twentyeleven'), get_comment_date(), get_comment_time())
+                                )
+                            );
+                            ?>
+
+                            <?php edit_comment_link(__('Edit', 'twentyeleven'), '<span class="edit-link">', '</span>'); ?>
+                        </div>
+                        <!-- .comment-author .vcard -->
+
+                        <?php if ($comment->comment_approved == '0') : ?>
+                        <p><em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.'); ?></em></p>
+                        <?php endif; ?>
+
+                    </footer>
+
+                    <div class="comment-content"><?php comment_text(); ?></div>
+
+                    <div class="reply">
+                        <?php comment_reply_link(array_merge($args, array('reply_text' => __('Reply'), 'depth' => $depth, 'max_depth' => $args['max_depth']))); ?>
+                    </div>
+                    <!-- .reply -->
+                </article><!-- #comment-## -->
+
+                    <?php
+                break;
+        endswitch;
+    }
+
+    ?>
