2e753224208b15fbf616a7e599c2031ca687e510
Simon Smith <simon@interlopers.net>
2012-12-21 00:28:56 +0000

    Add tumble beast 404 ftw

--- 404.php
+++ 404.php
@@ -1,1 +1,14 @@
-Fucked
+<?
+    get_header();
+?>
+
+    <section>
+        <h1 class="visuallyhidden">404! No page here I'm afraid...</h1>
+        <div style="margin: 30px 0 0">
+            <img src="<?= get_template_directory_uri(); ?>/assets/images/tb_sign1.png" alt="404 error message">
+        </div>
+    </section>
+
+<?
+    get_footer();
+?>
