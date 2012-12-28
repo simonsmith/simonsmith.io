a9c708bccd19a714d9f9fa641269c9f2460643b4
Simon Smith <simon@interlopers.net>
2012-12-28 21:47:28 +0000

    Reduce file size of 404

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@
     <section>
         <h1 class="visuallyhidden">404! No page here I'm afraid...</h1>
         <div style="margin: 30px 0 0">
-            <img src="<?= get_template_directory_uri(); ?>/assets/images/tb_sign1.png" alt="404 error message">
+            <img src="<?= get_template_directory_uri(); ?>/assets/images/404.jpg" alt="404 error message">
         </div>
     </section>
 
