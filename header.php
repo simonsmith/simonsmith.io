7baca589f65762db8c3eb654b0990f2c6bc00409
Simon Smith <simon@interlopers.net>
2013-01-25 01:22:18 +0000

    test 3

--- header.php
+++ header.php
@@ -28,7 +28,6 @@
         </script>
     </head>
     <body <? body_class(); ?>>
-<!-- a test here --> 
         <!--[if lt IE 8]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
