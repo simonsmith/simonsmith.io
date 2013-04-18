8f4c44b962d36ade5089499febac74759c1e3fb7
Simon Smith <simon@interlopers.net>
2013-04-18 22:30:40 +0100

    Make post tags a little lighter and swap to <dd>

--- assets/scripts/templates/partials/tags.js
+++ assets/scripts/templates/partials/tags.js
@@ -8,7 +8,7 @@ helpers = helpers || Handlebars.helpers; data = data || {};
 function program1(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n    <li><a href=\"";
+  buffer += "\n    <dd><a href=\"";
   if (stack1 = helpers.site_root) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.site_root; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
@@ -16,21 +16,21 @@ function program1(depth0,data) {
   if (stack1 = helpers.slug) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.slug; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "\" class=\"link-dark ajax\">";
+    + "\" class=\"link-lighter ajax\">";
   if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</a></li>\n";
+    + "</a></dd>\n";
   return buffer;
   }
 
-  buffer += "<ul class=\"post-tags\">\n";
+  buffer += "<dl class=\"post-tags\">\n    <dt class=\"visuallyhidden\">Post tags</dt>\n";
   options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data};
   if (stack1 = helpers.post_tags) { stack1 = stack1.call(depth0, options); }
   else { stack1 = depth0.post_tags; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   if (!helpers.post_tags) { stack1 = blockHelperMissing.call(depth0, stack1, options); }
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n</ul>\n";
+  buffer += "\n</dl>\n";
   return buffer;
   });
 });
