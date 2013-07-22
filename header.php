7a2570508aa3fe2a5e041bcc15b4614e0f20f42b
Simon Smith <simon@interlopers.net>
2013-07-22 23:50:05 +0100

    Move GA to footer

--- header.php
+++ header.php
@@ -11,18 +11,6 @@
         <meta charset="utf-8">
         <meta name="description" content="<?= get_bloginfo('description', 'display') ?>">
         <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/assets/css/site.css">
-        <script>document.cookie='resolution='+screen.width+("devicePixelRatio" in window ? ","+devicePixelRatio : ",1")+'; path=/';</script>
-        <script type="text/javascript">
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-6798894-3']);
-            _gaq.push(['_trackPageview']);
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-        </script>
         <script src="<?= $script_path->getPath() ?>/lib/modernizr.js"></script>
         <script>
             var require = {
