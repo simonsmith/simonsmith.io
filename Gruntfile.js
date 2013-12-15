5d4080a0ce6ee1d9794a5f1e3973c48cc4fbf3f4
Simon Smith <simon@interlopers.net>
2013-12-15 19:47:55 +0000

    Add tasks for deploying

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,13 @@ module.exports = function(grunt) {
         watch: {
             css: {
                 files: 'assets/sass/**/*.scss',
-                tasks: ['sass:dev', 'autoprefixer']
+                tasks: ['css']
             },
             html: {
                 files: [
                     '_layouts/*.html',
                     '_includes/*.html',
+                    'assets/css/*.css',
                     '_data/*.yml',
                     'blog/_posts/*.md',
                     'projects/_posts/*.md',
@@ -26,6 +27,19 @@ module.exports = function(grunt) {
             build: {
                 src: 'assets/css/site.css',
                 dest: 'assets/css/site.css'
+            }
+        },
+
+        concat: {
+            css: {
+                files: {
+                    'assets/css/site.css': [
+                        'bower_components/normalize-css/normalize.css',
+                        'bower_components/syntax.css/syntax.css',
+                        'bower_components/suit-**/*.css',
+                        'assets/css/sass-compiled.css'
+                    ]
+                }
             }
         },
 
@@ -46,12 +60,7 @@ module.exports = function(grunt) {
                     keepSpecialComments: 0
                 },
                 files: {
-                    'assets/css/site.css': [
-                        'bower_components/normalize-css/normalize.css',
-                        'bower_components/syntax.css/syntax.css',
-                        'bower_components/suit-**/*.css',
-                        'assets/css/sass-compiled.css'
-                    ]
+                    '_site/assets/css/site.css': '_site/assets/css/site.css'
                 }
             }
         },
@@ -65,16 +74,31 @@ module.exports = function(grunt) {
                     'assets/css/sass-compiled.css': ['assets/sass/site.scss']
                 }
             }
+        },
+
+        'ftp-deploy': {
+            deploy: {
+                auth: {
+                    host: 'simonsmith.io',
+                    port: 21,
+                    authKey: 'me'
+                },
+                src: './_site',
+                dest: '/public_html'
+            }
         }
     });
 
     grunt.registerTask('css', [
         'sass:dev',
-        'autoprefixer'
+        'autoprefixer',
+        'concat:css'
     ]);
 
     grunt.registerTask('default', [
         'css',
-        'jekyll:build'
+        'jekyll:build',
+        'cssmin:build',
+        'ftp-deploy'
     ]);
 };
