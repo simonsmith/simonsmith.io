8c1c30b3a5c35c6cc5dc49295b90ea5ab4c175e4
Simon Smith <me@simonsmith.io>
2015-04-13 21:14:50 +0100

    Remove need for require-dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,10 @@
-require('require-dir')('./gulp/tasks', {
-  recurse: true
+var fs   = require('fs');
+var path = require('path');
+
+var taskDir = path.resolve('./gulp/tasks');
+var tasks = fs.readdirSync(taskDir);
+
+tasks.forEach(function(file) {
+  require(path.join(taskDir, file))
 });
+
