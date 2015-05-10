168e8f532997ff29e7aeb7f07a7ee8dbcb957028
Simon Smith <me@simonsmith.io>
2015-05-10 17:58:45 +0100

    Remove asset cache busting

--- config.rb
+++ config.rb
@@ -47,7 +47,7 @@ configure :build do
   activate :minify_javascript
 
   # Enable cache buster
-  activate :asset_hash
+  # activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
