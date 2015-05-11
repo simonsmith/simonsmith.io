72a80a19640ce8241980e2ae9400203d92a18113
Simon Smith <me@simonsmith.io>
2015-05-11 14:07:21 +0100

    Simplify postcss task to use one stream

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp    = require('gulp');
 var postcss = require('gulp-postcss');
+var filter  = require('gulp-filter');
 var rename  = require('gulp-rename');
 var plugins = require('postcss-load-plugins')();
 
@@ -30,27 +31,21 @@ var processors = [
   plugins.logWarnings()
 ];
 
-/**
- * Lint the files individually
- */
-gulp.task('lint', function() {
-  return gulp.src(['source/stylesheets/src/**/*.css', '!source/stylesheets/src/theme.css'])
+gulp.task('postcss', function() {
+  return gulp.src('source/stylesheets/src/**/*.css')
     .pipe(postcss([
-      // Parse the nested rules first so linters don't choke
+      // Parse the nested rules first so bemLinter doesn't choke
       plugins.nested(),
       plugins.bemLinter('suit'),
       plugins.logWarnings()
-    ]));
-});
-
-gulp.task('postcss', function() {
-  return gulp.src('source/stylesheets/src/index.css')
+    ]))
+    .pipe(filter(['index.css']))
     .pipe(postcss(processors))
     .pipe(rename('_components.css'))
-    .pipe(gulp.dest('source/stylesheets/dist'));
+    .pipe(gulp.dest('source/stylesheets/dist'))
 });
 
-gulp.task('css', ['lint', 'postcss']);
+gulp.task('css', ['postcss']);
 gulp.task('default', ['css']);
 gulp.task('watch', function() {
   gulp.watch('source/stylesheets/src/**/*.css', ['css']);
