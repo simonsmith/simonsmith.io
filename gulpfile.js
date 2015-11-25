0ccdb56a9136e6951fce8a1420aeb5da5fb7490a
Simon Smith <me@simonsmith.io>
2015-11-25 23:55:11 +0000

    Remove postcss from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,9 @@
-var gulp     = require('gulp');
-var postcss  = require('gulp-postcss');
-var filter   = require('gulp-filter');
-var rename   = require('gulp-rename');
-var svgstore = require('gulp-svgstore');
-var svgmin   = require('gulp-svgmin');
-var plugins  = require('postcss-load-plugins')();
-var autoprefixer = require('autoprefixer');
+const gulp = require('gulp');
+const svgstore = require('gulp-svgstore');
+const rename = require('gulp-rename');
+const svgmin = require('gulp-svgmin');
 
-var processors = [
-  plugins.import({
-    path: 'source/stylesheets/src/'
-  }),
-  plugins.customProperties(),
-  plugins.calc(),
-  plugins.size,
-  plugins.customMedia(),
-  plugins.propertyLookup(),
-  plugins.pxtorem({
-    prop_white_list: [
-      'font',
-      'font-size',
-      'margin',
-      'margin-top',
-      'margin-right',
-      'margin-bottom',
-      'margin-left',
-      'padding',
-      'padding-top',
-      'padding-right',
-      'padding-bottom',
-      'padding-left'
-    ]
-  }),
-  autoprefixer(),
-  plugins.nested(),
-  plugins.logWarnings()
-];
-
-gulp.task('postcss', function() {
-  return gulp.src('source/stylesheets/src/**/*.css')
-    .pipe(postcss([
-      // Parse the nested rules first so bemLinter doesn't choke
-      plugins.nested(),
-      plugins.bemLinter(),
-      plugins.logWarnings()
-    ]))
-    .pipe(filter(['index.css']))
-    .pipe(postcss(processors))
-    .pipe(rename('_components.css'))
-    .pipe(gulp.dest('source/stylesheets/dist'));
-});
-
-gulp.task('icons', function() {
+gulp.task('icons', () => {
   return gulp.src(['source/svg/*.svg'])
     .pipe(rename({ prefix: 'icon-' }))
     .pipe(svgmin())
@@ -60,10 +12,4 @@ gulp.task('icons', function() {
     }))
     .pipe(rename('icons.svg'))
     .pipe(gulp.dest('source/icons'));
-});
-
-gulp.task('css', ['postcss']);
-gulp.task('default', ['css']);
-gulp.task('watch', function() {
-  gulp.watch('source/stylesheets/src/**/*.css', ['css']);
 });
