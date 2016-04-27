1fb5c65f251cab03dc372098e8b06c4822d8557c
Simon Smith <me@simonsmith.io>
2016-04-27 22:35:22 +0100

    Switch back to postcss-cli

--- source/stylesheets/config.js
+++ source/stylesheets/config.js
@@ -1,17 +1,16 @@
-const postcss = require('postcss');
-const nested = require('postcss-nested');
-
 module.exports = {
-  beforeLint(css) {
-    return postcss([nested]).process(css).css;
-  },
   use: [
+    'postcss-import',
+    'postcss-nested',
+    'postcss-custom-properties',
+    'postcss-calc',
+    'postcss-custom-media',
     'postcss-property-lookup',
     'postcss-size',
     'autoprefixer',
     'postcss-reporter'
   ],
-  "postcss-import": {
+  'postcss-import': {
     'path': 'source/stylesheets/src'
   }
 };
