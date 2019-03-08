18ca31453ae64ed50fa3f4ce04b3273be6e75054
Simon Smith <simonsmith@users.noreply.github.com>
2019-03-08 10:39:25 +0000

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,16 +65,9 @@ module.exports = {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'gatsby-starter-default',
-        short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/site-icon.png',
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
 };
